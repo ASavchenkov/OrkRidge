0d8aeda4f1ebec9971ae63cdb267dea4278d9c5a
ASavchenkov <asavchenkov1@gmail.com>
2021-12-27 17:56:23 -0800

    switched to farm model

---
+++ conf/local.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Minimal local config
+ */
+$conf['useacl'] = 1;
+$conf['superuser'] = '@admin';
+
+$conf['title'] = 'OrkRidge';
+
+$conf['disableactions'] = 'register,register';
---
+++ conf/users.auth.php
@@ -0,0 +1,3 @@
+# <?php exit()?>
+admin:$2y$10$MNSGcBfQC0aFNb3vL8Ahpeme827GZJxZJtPPMM1vV3uF4YF7w7syG:admin:youremail@gmail.com:admin,user
+
